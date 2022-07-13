be321a1bdc8490f45d1bc587fddd4a3805224b5f
NunesNathan <nunesxd7@hotmail.com>
2022-07-13 11:59:43 -0300

    test: car create controller test

---
+++ src/tests/unit/controllers/car.test.ts
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+import sinon, { SinonStub } from 'sinon';
+import { Car } from '../../../interfaces/CarInterface';
+import IErrorHttp from '../../../middleware/error/errorHttp';
+import CarService from '../../../services/carService';
+import CarController from '../../../controllers/CarController';
+import * as mock from '../../mocks/carMocks';
+import CarModel from '../../../models/CarModel';
+import { Request, Response, NextFunction } from 'express';
+import { afterEach } from 'mocha';
+
+describe('Car Controller', () => {
+  describe('Create Car', () => {
+    const req = {} as Request;
+    const res = {} as Response;
+    let next: NextFunction = () => {};
+    const carService = new CarService(new CarModel());
+    before(() => {
+      res.status = sinon.stub().returns(res)
+      res.json = sinon.stub().returns(res)
+    });
+
+    afterEach(() => {
+      (carService.create as SinonStub).restore();
+    });
+
+    it('Success case', async () => {
+      sinon.stub(carService, 'create').resolves({ data: mock.responseValidCar, status: 201 });
+
+      const carController = new CarController(carService);
+
+      await carController.create(req, res, next);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(mock.responseValidCar)).to.be.true;
+    });
+
+    it('Fail case', async () => {
+      const err = new IErrorHttp(400, 'year is required');
+      sinon.stub(carService, 'create').throwsException(err);
+
+      const carController = new CarController(carService);
+      try {
+        await carController.create(req, res, next);
+      } catch (error) {
+        expect(error).instanceOf(IErrorHttp);
+        expect((next as sinon.SinonStub).calledWith(400, 'year is requireed')).to.be.true;
+      }
+    });
+  });
+});
