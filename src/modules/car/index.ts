eb0dce7bc09c077a61a23309db838b4f96100ea3
NunesNathan <nunesxd7@hotmail.com>
2022-07-13 14:09:26 -0300

    feat: get one car route works

--- src/modules/car/index.ts
+++ src/modules/car/index.ts
@@ -3,6 +3,7 @@ import carController from './carUseCases';
 const car = {
   create: carController.create.bind(carController),
   getAll: carController.read.bind(carController),
+  getOne: carController.readOne.bind(carController),
 };
 
 export default car;
--- src/router/carRouter.ts
+++ src/router/carRouter.ts
@@ -4,6 +4,7 @@ import car from '../modules/car';
 const router = Router();
 
 router.post('/', car.create)
-  .get('/', car.getAll);
+  .get('/', car.getAll)
+  .get('/:id', car.getOne);
 
 export default router;
